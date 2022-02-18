98b66ccacae720bf89ae79b6fd12081f0068e8c2
daniel_shtel <97levchenko.danya@gmail.com>
2022-02-18 22:48:03 +0200

    add models & users list endpoint

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-import fastapi
+import os
+from typing import List
+
+from fastapi import FastAPI
+from sqlmodel import create_engine, Session
+
+from models.user import User, create_db_and_tables, UserBase
+import uvicorn
+from ramda import *
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+engine = create_engine(f'sqlite:///{ROOT_DIR}/api.db')
+
+session = Session(engine)
+app = FastAPI()
+
+
+
+@app.on_event('startup')
+async def on_startup():
+    await create_db_and_tables(connection=engine)
+
+
+@app.get('/login')
+async def login():
+    pass
+
+
+@app.post('/create_user/', response_model=User)
+async def create_user(user: UserBase):
+    return await user.create(session=session)
+
+
+@app.get('/users', response_model=List[User])
+async def users():
+    user_list = await User.get_list(session=session)
+    return user_list
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=80, reload=True)
---
+++ models/user.py
@@ -0,0 +1,57 @@
+import os
+from typing import Optional
+
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+
+class UserBase(SQLModel):
+
+    name: str = Field(default=0, index=True)
+    email: str = Field(default=0, index=True)
+    age: int = Field(default=0, index=True)
+
+    async def create(self, session: Session):
+        with session:
+            user = User.from_orm(self)
+            session.add(user)
+            session.commit()
+            session.refresh(user)
+            return user
+
+
+class User(UserBase, table=True):
+    __tablename__ = 'user'
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    class Config:
+        orm_mode = True
+
+    @classmethod
+    async def get_list(cls, session: Session):
+        return session.exec(select(cls)).all()
+
+
+class UserRead(SQLModel):
+    id: int
+    name: str
+    email: str
+    age: Optional[int] = None
+
+
+async def create_db_and_tables(connection):
+    SQLModel.metadata.create_all(connection)
+
+
+if __name__ == '__main__':
+    pass
+    from time import sleep
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(f'sqlite:///{ROOT_DIR}/api.db', connect_args=connect_args)
+    SQLModel.metadata.drop_all(engine)
+    sleep(1)
+    SQLModel.metadata.create_all(engine)
+    sleep(1)
