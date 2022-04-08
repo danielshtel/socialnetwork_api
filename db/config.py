1253194598f75d7e5e7e79321a8bd2e9b9ee193f
Daniil Levchenko <97levchenko.danya@gmail.com>
2022-04-09 01:56:35 +0300

    [upd] refactor logic

--- db/config.py
+++ db/config.py
@@ -8,6 +8,4 @@ engine = create_engine(f'sqlite:///{ROOT_DIR}/api.db')
 session = Session(engine)
 
 if __name__ == '__main__':
-    from models.user import User
-    from models.post import Post
-    SQLModel.metadata.drop_all(engine)
+    pass
