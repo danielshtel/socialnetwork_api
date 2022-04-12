86c989b2f694329274e72ed50388410604802507
Daniil Levchenko <97levchenko.danya@gmail.com>
2022-04-13 02:15:10 +0300

    [add]prefix 'api' -> api/__init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from .user import router as user_router
 from .post import router as post_router
 
-router = APIRouter()
+router = APIRouter(prefix='/api')
 router.include_router(user_router)
 router.include_router(post_router)
